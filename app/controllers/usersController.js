8d684269c8a01c0c165db9fc8bd0b8a973b471ad
RTBathula <battu.network@gmail.com>
2015-12-12 19:36:41 -0300

    partially done

--- app/controllers/usersController.js
+++ app/controllers/usersController.js
@@ -12,10 +12,148 @@ userService){
  
   $rootScope.isFullScreen=false;
   $rootScope.page='users';  
+
+  //Users Page Specific
+  $scope.newUser={
+    name:null,
+    email:null,
+    password:null,
+    isAdmin:false
+  };
+  $scope.addUserSpinner=false;
+
+  $scope.usersList=[];
+  $scope.skip=0;
+  $scope.limit=15;
+  $scope.loadingUsers=false;
+  $scope.updatingUser=[];
  
   $scope.init= function() {  
-    $rootScope.pageHeaderDisplay="Manage Users";  
+    $rootScope.pageHeaderDisplay="Manage Users";
+    getUserBySkipLimit($scope.skip,$scope.limit);  
   }; 
+
+  $scope.addUser=function(){
+    if($scope.newUser.name && $scope.newUser.email && $scope.newUser.password){
+      $scope.addUserSpinner=true;    
+      
+      userService.signUp($scope.newUser.name,$scope.newUser.email,$scope.newUser.password,$scope.newUser.isAdmin)             
+      .then(function(data){        
+        return userService.getUserByIdByAdmin($scope.newUser.email);      
+      }).then(function(addedUser){
+        $scope.addUserSpinner=false; 
+        $scope.newUser={
+          name:null,
+          email:null,
+          password:null,
+          isAdmin:false
+        };
+        
+        if(addedUser){
+          $scope.usersList.unshift(addedUser);
+        }
+        
+      },function(error){
+        $scope.addUserSpinner=false;
+        errorNotify(error);
+      });      
+    }
+  };
+
+
+  $scope.updateUserActive=function(userId,isActive,index){
+    if($rootScope.user._id!=userId){
+      var activate;
+      if(isActive){
+        activate=false;
+      }else if(!isActive){
+        activate=true;
+      }
+
+      $scope.updatingUser[index]=true; 
+      userService.updateUserActive(userId,activate)
+      .then(function(user){       
+         $scope.updatingUser[index]=false;  
+      },function(error){
+        errorNotify(error);
+        $scope.updatingUser[index]=false;               
+      });
+
+    }else{
+      errorNotify("You can't perfom this action on yourself!");
+      isActive=isActive;
+    }
+    
+  };
+
+  $scope.updateUserRole=function(userId,isAdmin,index){
+    if($rootScope.user._id!=userId){
+      if(isAdmin=="true"){
+        isAdmin=true;
+      }else{
+        isAdmin=false;
+      }
+
+      $scope.updatingUser[index]=true; 
+      userService.updateUserRole(userId,isAdmin)
+      .then(function(user){       
+         $scope.updatingUser[index]=false;  
+      },function(error){
+        errorNotify(error);
+        $scope.updatingUser[index]=false;               
+      });
+    }else{
+      errorNotify("You can't perfom this action on yourself!");
+    }
+  };
+
+   $scope.deleteUser=function(userId,index){    
+    if($rootScope.user._id!=userId){
+      $scope.updatingUser[index]=true; 
+      userService.deleteUser(userId)
+      .then(function(resp){ 
+        $scope.usersList.splice(index,1);      
+        $scope.updatingUser[index]=false;  
+      },function(error){
+        errorNotify(error);
+        $scope.updatingUser[index]=false;               
+      });
+    }else{
+      errorNotify("You can't perfom this action on yourself!");
+    }
+  };
+
+  $scope.loadMoreUsers=function(){    
+    $scope.loadingUsers=true;
+    userService.getUserBySkipLimit($scope.limit,$scope.limit+10)
+    .then(function(userList){ 
+      if(userList.length>0){
+
+        if($scope.usersList.length>0){
+          $scope.usersList = $scope.usersList.concat(userList);           
+        }else{
+          $scope.usersList=userList;
+        }
+        
+      }  
+      $scope.loadingUsers=false;   
+    },function(error){
+      errorNotify(error);
+      $scope.loadingUsers=false;              
+    });
+  };
+
+  /********Private Functions**************/
+  function getUserBySkipLimit(skip,limit){
+    userService.getUserBySkipLimit(skip,limit)
+    .then(function(userList){ 
+      if(userList.length>0){
+        $scope.usersList=userList;
+      }          
+    },function(error){
+      errorNotify(error);                 
+    });
+  }
 
   //Notification
   function errorNotify(errorMsg){
--- app/services/userService.js
+++ app/services/userService.js
@@ -3,6 +3,31 @@ angular.module('CloudBoostDashboard')
 
     var global = {};
 
+    global.signUp=function(name,email,password,isAdmin){
+      var q=$q.defer();
+      $http.post(serverURL+'/user/signup', {name:name,email:email,password:password,isAdmin:isAdmin}).
+        success(function(data, status, headers, config) {
+              q.resolve(data);
+
+              if(!__isDevelopment){
+                /****Tracking*********/                
+                 mixpanel.alias(data._id);
+
+                 mixpanel.people.set({ "Name": data.name,"$email": data.email});
+                 //mixpanel.identify(data._id);
+
+                 mixpanel.register({ "Name": data.name,"Email": data.email});
+                 mixpanel.track('AdminUserSignup', { "Name": data.name,"Email": data.email});
+                /****End of Tracking*****/
+              }
+              
+        }).
+        error(function(data, status, headers, config) {
+            q.reject(data);
+        });
+      return q.promise;
+    }
+
      global.logOut = function(){
         var q=$q.defer();
 
@@ -31,6 +56,81 @@ angular.module('CloudBoostDashboard')
        
         return  q.promise;
       };
+
+      global.getUserBySkipLimit = function(skip,limit){
+        var q=$q.defer();       
+
+        $http.get(serverURL+'/user/list/'+skip+'/'+limit).
+         success(function(data, status, headers, config) {
+            q.resolve(data);
+         }).
+         error(function(data, status, headers, config) {
+            q.reject(data);
+         });
+       
+        return  q.promise;
+      };
+
+      global.updateUserActive = function(userId,isActive){
+        var q=$q.defer();       
+
+        $http.get(serverURL+'/user/active/'+userId+'/'+isActive).
+         success(function(data, status, headers, config) {
+            q.resolve(data);
+         }).
+         error(function(data, status, headers, config) {
+            q.reject(data);
+         });
+       
+        return  q.promise;
+      };
+
+       global.updateUserRole = function(userId,isAdmin){
+            var q=$q.defer();       
+
+            $http.get(serverURL+'/user/changerole/'+userId+'/'+isAdmin).
+             success(function(data, status, headers, config) {
+                q.resolve(data);
+             }).
+             error(function(data, status, headers, config) {
+                q.reject(data);
+             });
+           
+            return  q.promise;
+       };
+
+
+        global.deleteUser = function(userId){
+            var q=$q.defer();
+            $http.delete(serverURL+'/user/'+userId).
+            success(function(data, status, headers, config) { 
+                q.resolve(status);                 
+            }).
+            error(function(data, status, headers, config) {
+                q.reject(status);
+                if(status===401){
+                  $rootScope.logOut();
+                }
+            });
+
+            return  q.promise;
+        };
+
+        global.getUserByIdByAdmin = function(email){
+            var q=$q.defer();
+            $http.post(serverURL+'/user/byadmin',{email:email}).
+            success(function(data, status, headers, config) { 
+                q.resolve(data);                 
+            }).
+            error(function(data, status, headers, config) {
+                q.reject(status);
+                if(status===401){
+                  $rootScope.logOut();
+                }
+            });
+
+            return  q.promise;
+        };
 
       global.updateUserInfo = function(name,oldPassword,newPassword){
         var q=$q.defer(); 
