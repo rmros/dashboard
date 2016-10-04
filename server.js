e0f7a0c8b1bf13e294c880c171596d489a7e2ad3
Nawaz Dhandala <nawazdhandala@outlook.com>
2016-10-04 12:04:25 +0530

    Config file is dynamically generated

--- server.js
+++ server.js
@@ -2,6 +2,38 @@
 var express = require('express');
 var app = express();
 
+app.get('/app/key.js',function(req,res){
+	res.setHeader('Content-type', 'text/plain');
+	res.charset = 'UTF-8';
+	var content= "";
+
+	/***************************************************Connecting URLs*********************************************************/
+	content+= "var __isDevelopment = "+process.env["CLOUDBOOST_DEVELOPMENT"] || "false"+";\n";
+	
+
+	content+= "var frontendServerURL = null,\n";
+	           content+= "SERVER_URL = null,\n";
+	           content+= "landingURL = null,\n";
+	         content+= "analyticsURL = null;\n";
+
+	content+= "landingURL = 'https://www.cloudboost.io';\n";
+
+
+	content+= "if(window.location.hostname==='dashboard.cloudboost.io'){\n";
+	    content+= "frontendServerURL='https://service.cloudboost.io';\n";
+	    content+= "SERVER_URL='https://api.cloudboost.io';\n";
+	content+= "}else if(window.location.hostname==='beta-dashboard.cloudboost.io'){\n";
+	    content+= "frontendServerURL='https://beta-service.cloudboost.io';\n";
+	    content+= "SERVER_URL='https://beta-api.cloudboost.io';\n";
+	content+= "}else{\n";
+	    content+= "frontendServerURL = window.location.protocol+'//'+window.location.hostname + ':3000';\n";
+	    content+= "SERVER_URL =  window.location.protocol+'//'+window.location.hostname + ':4730';\n";
+	content+= "}\n";
+
+		res.write(content);
+		res.end();
+	});
+
 app.use(express.static(__dirname));
 
 app.set('port', process.env.PORT || 1440);
