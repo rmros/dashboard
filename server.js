126a9c235f3c0c4b3a86a1eaa06b7e87cf596e40
Nawaz Dhandala <nawazdhandala@outlook.com>
2016-10-12 16:20:32 +0530

    Accounts env variable added

--- server.js
+++ server.js
@@ -8,26 +8,29 @@ app.get('/app/key.js',function(req,res){
 	var content= "";
 
 	/***************************************************Connecting URLs*********************************************************/
-	content+= "var __isDevelopment = "+(process.env["CLOUDBOOST_DEVELOPMENT"] || "false")+";\n";
-	
+		content+= "var __isDevelopment = "+(process.env["CLOUDBOOST_DEVELOPMENT"] || "false")+";\n";
 
-	content+= "var frontendServerURL = null,\n";
-	           content+= "SERVER_URL = null,\n";
-	           content+= "landingURL = null,\n";
-	         content+= "analyticsURL = null;\n";
 
-	content+= "landingURL = 'https://www.cloudboost.io';\n";
+		content+= "var frontendServerURL = null,\n";
+		content+= "SERVER_URL = null,\n";
+		content+= "landingURL = null,\n";
+		content+= "analyticsURL = null;\n";
+		content+= "ACCOUNTS_URL=null;"
 
-	content+= "if(window.location.hostname==='dashboard.cloudboost.io'){\n";
-	    content+= "frontendServerURL='https://service.cloudboost.io';\n";
-	    content+= "SERVER_URL='https://api.cloudboost.io';\n";
-	content+= "}else if(window.location.hostname==='beta-dashboard.cloudboost.io'){\n";
-	    content+= "frontendServerURL='https://beta-service.cloudboost.io';\n";
-	    content+= "SERVER_URL='https://beta-api.cloudboost.io';\n";
-	content+= "}else{\n";
-	    content+= "frontendServerURL = window.location.protocol+'//'+window.location.hostname + ':3000';\n";
-	    content+= "SERVER_URL =  window.location.protocol+'//'+window.location.hostname + ':4730';\n";
-	content+= "}\n";
+		content+= "landingURL = 'https://www.cloudboost.io';\n";
+
+		content+= "if(window.location.hostname==='dashboard.cloudboost.io'){\n";
+		content+= "ACCOUNTS_URL='https://accounts.cloudboost.io';\n";
+		content+= "frontendServerURL='https://service.cloudboost.io';\n";
+		content+= "SERVER_URL='https://api.cloudboost.io';\n";
+		content+= "}else if(window.location.hostname==='beta-dashboard.cloudboost.io'){\n";
+		content+= "frontendServerURL='https://beta-service.cloudboost.io';\n";
+		content+= "SERVER_URL='https://beta-api.cloudboost.io';\n";
+		content+= "}else{\n";
+		content+= "frontendServerURL = window.location.protocol+'//'+window.location.hostname + ':3000';\n";
+		content+= "SERVER_URL =  window.location.protocol+'//'+window.location.hostname + ':4730';\n";
+		content+= "ACCOUNTS_URL= window.location.protocol+'//'+window.location.hostname + ':1447';\n";
+		content+= "}\n";
 
 		res.write(content);
 		res.end();
